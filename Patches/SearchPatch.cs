b43a932e8aba861225e145b53660e490e57c278e
Sergej Timofeev <sergej.by@gmail.com>
2025-03-29 23:06:15 +0300

    files

---
+++ Patches/SearchPatch.cs
@@ -0,0 +1,120 @@
+﻿using EFT;
+using EFT.InventoryLogic;
+using EFT.UI.DragAndDrop;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LootInstantSearch.Patches
+{
+    internal class SearchPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {   
+            
+            return AccessTools.Method(typeof(PlayerSearchControllerClass), nameof(PlayerSearchControllerClass.IsItemKnown));
+        }
+
+        [PatchPrefix]
+        static void Prefix(ref PlayerSearchControllerClass __instance, Item item)
+        {
+            __instance.SetItemAsKnown(item, false);                     
+        }
+
+
+    }
+
+    internal class SearchPatch2 : ModulePatch 
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(SearchableView), nameof(SearchableView.method_2));
+        }
+
+        [PatchPrefix]
+        static void Prefix(SearchableView __instance, IPlayerSearchController ___iplayerSearchController_0, SearchableItemItemClass ___searchableItemItemClass)
+        {
+            ___iplayerSearchController_0.SetItemAsKnown(___searchableItemItemClass, false);
+            
+        }
+
+    }
+
+    internal class SearchPatch3 : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(SearchableView), nameof(SearchableView.Start));
+        }
+
+        [PatchPrefix]
+        static bool Prefix(SearchableView __instance)
+        {
+            __instance.method_3();
+            return false;
+        }
+
+    }
+
+    internal class SearchPatch4 : ModulePatch  
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(GClass2002), nameof(GClass2002.ContainsUnknownItems));
+        }
+
+        [PatchPrefix]
+        static bool Prefix(ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
+
+    }
+
+    internal class SearchPatch5 : ModulePatch 
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(GClass2002), nameof(GClass2002.SearchContents));
+        }
+
+        [PatchPrefix]
+        static void Prefix(ref GClass2002 __instance, SearchableItemItemClass searchableItem)
+        {
+            __instance.SetItemAsSearched(searchableItem);
+        }
+
+    }
+
+    internal class AttentionPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+
+            return AccessTools.Method(typeof(SkillManager), nameof(SkillManager.method_2));
+        }
+
+
+        [PatchPostfix]
+        static void Postfix(ref SkillManager __instance)
+        {
+            try
+            {
+                __instance.AttentionEliteLuckySearch.Elite(0f).PerLevel(0f);
+            }
+            catch (Exception ex)
+            {
+                throw Plugin.ShowErrorNotif(ex);
+            }
+        }
+
+    }
+
+}
---
+++ Plugin.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LootInstantSearch.Patches;
+
+namespace LootInstantSearch
+{
+    // first string below is your plugin's GUID, it MUST be unique to any other mod. Read more about it in BepInEx docs. Be sure to update it if you copy this project.
+    [BepInPlugin("wizard.LootInstantSearch", "LootInstantSearch", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource LogSource;
+
+        // BaseUnityPlugin inherits MonoBehaviour, so you can use base unity functions like Awake() and Update()
+        private void Awake()
+        {
+            // save the Logger to variable so we can use it elsewhere in the project
+            LogSource = Logger;
+            LogSource.LogInfo("LootInstantSearch loaded!");
+
+            // uncomment line(s) below to enable desired example patch, then press F6 to build the project:
+            new AttentionPatch().Enable();
+            new SearchPatch().Enable();
+            new SearchPatch2().Enable();
+            new SearchPatch3().Enable();
+            new SearchPatch4().Enable();
+            new SearchPatch5().Enable();
+            
+        }
+
+        private static HashSet<string> AlreadyThrownPatches = new HashSet<string>();
+        public static Exception ShowErrorNotif(Exception ex)
+        {
+            if (!AlreadyThrownPatches.Add(ex.Source))
+            {
+                return ex;
+            }
+
+            NotificationManagerClass.DisplayWarningNotification(
+                $"Seion.Iof thew an exception. Perhaps version incompatibility? Exception: {ex.Message}",
+                duration: EFT.Communications.ENotificationDurationType.Infinite
+                );
+
+            return ex;
+        }
+    }
+}
